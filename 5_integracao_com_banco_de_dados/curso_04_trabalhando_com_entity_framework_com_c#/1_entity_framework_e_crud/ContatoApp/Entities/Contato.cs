4e8e0eee5a381cbacbc67956795d00171333a014
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-08-31 19:04:59 -0300

    entendendo as migrations

--- 5_integracao_com_banco_de_dados/curso_04_trabalhando_com_entity_framework_com_c#/1_entity_framework_e_crud/ContatoApp/Entities/Contato.cs
+++ 5_integracao_com_banco_de_dados/curso_04_trabalhando_com_entity_framework_com_c#/1_entity_framework_e_crud/ContatoApp/Entities/Contato.cs
@@ -1,9 +1,21 @@
-﻿namespace ContatoApp;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContatoApp;
 
 public class Contato
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório.")] 
+    [StringLength(200, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 150 caracteres.")]
     public string Nome { get; set; } = default!;
+
+    [Required]
+    [Phone]
     public string Telefone { get; set; } = default!;
+
     public bool Ativo { get; set; } = default!;
 }
