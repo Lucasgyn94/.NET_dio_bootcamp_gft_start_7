188611ca619111061127d29f104c4c905847effd
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-08-31 21:02:43 -0300

    criação do endpoint de update

--- 5_integracao_com_banco_de_dados/curso_04_trabalhando_com_entity_framework_com_c#/1_entity_framework_e_crud/ContatoApp/Controllers/ContatoController.cs
+++ 5_integracao_com_banco_de_dados/curso_04_trabalhando_com_entity_framework_com_c#/1_entity_framework_e_crud/ContatoApp/Controllers/ContatoController.cs
@@ -15,13 +15,27 @@ public class ContatoController : ControllerBase
     }
 
     [HttpPost]
-    public IActionResult Create(Contato contato)
+    public IActionResult Criar(Contato contato)
     {
         this._context.Add(contato);
         this._context.SaveChanges();
         return Ok(contato);
     }
 
+    [HttpGet]
+    public IActionResult ObterTodos()
+    {
+        var contato = this._context.Contatos.ToList();
+
+        if (contato == null)
+        {
+            return NotFound();
+        }
+
+        this._context.SaveChanges();
+
+        return Ok(contato);
+    }
     [HttpGet("{id}")]
     public IActionResult ObterPorId(int id)
     {
@@ -33,6 +47,27 @@ public class ContatoController : ControllerBase
         }
 
         return Ok(contato);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Atualizar(int id, Contato contato)
+    {
+        var contatoBanco = this._context.Contatos.Find(id);
+
+        if (contatoBanco == null)
+        {
+            return NotFound();
+        }
+
+        contatoBanco.Nome = contato.Nome;
+        contatoBanco.Telefone = contato.Telefone;
+        contatoBanco.Ativo = contato.Ativo;
+
+        this._context.Update(contatoBanco);
+
+        this._context.SaveChanges();
+
+        return Ok(contatoBanco);
     }
 
 }
